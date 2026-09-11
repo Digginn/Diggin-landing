a68a46f3d94f6642cd3c960c7bd74964b3e8103d
Sumin <tnalsdldkr2@naver.com>
2026-09-11 18:14:40 +0900

    chore: 에셋 추가 및 파비콘 변경

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,21 @@
-import "./globals.css";
+import './globals.css'
 
 export const metadata = {
-  title: "DIGGIN",
-  description: "쇼핑 아카이빙 앱, 찜했던 그 상품 다시 찾아드릴게요.",
-};
+  title: 'Diggin',
+  description: '쇼핑 아카이빙 앱, 찜했던 그 상품 다시 찾아드릴게요.',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ko" className="h-full">
+    <html lang='ko' className='h-full'>
       <head>
-        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
+        <link rel='preconnect' href='https://cdn.jsdelivr.net' />
         <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+          rel='stylesheet'
+          href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css'
         />
       </head>
-      <body className="min-h-full">{children}</body>
+      <body className='min-h-full'>{children}</body>
     </html>
-  );
+  )
 }
