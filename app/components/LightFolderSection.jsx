72fc0d56664a8877c889df594cbd44e6a17efca1
Sumin <tnalsdldkr2@naver.com>
2026-09-13 00:08:17 +0900

    fix: 폴더 역재생 시 sticky 복귀 조건을 sectionBottom 기준으로 수정

--- app/components/LightFolderSection.jsx
+++ app/components/LightFolderSection.jsx
@@ -201,8 +201,7 @@ export default function LightFolderSection() {
             // relativeTop = end - sectionTop (section이 stage의 기준점이므로)
             stageEl.style.top = `${end - top}px`
           }
-        } else if (naturalScrollRef.current && nextProgress < 0.95) {
-          // 충분히 위로 스크롤한 경우에만 sticky 복귀 (경계값 진동 방지)
+        } else if (naturalScrollRef.current && sectionBottom > stickyCutoff + 4) {
           naturalScrollRef.current = false
           stageEl.style.position = ''
           stageEl.style.top = ''
